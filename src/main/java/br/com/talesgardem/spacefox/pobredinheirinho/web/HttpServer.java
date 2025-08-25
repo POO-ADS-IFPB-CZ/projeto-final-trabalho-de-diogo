8018eb662b2fc4d10ade971b5525d29699fdcee7
Space_Fox <44732812+SpaceFox1@users.noreply.github.com>
2025-08-25 13:59:21 -0300

    feat: initial HttpServer implementation

---
+++ src/main/java/br/com/talesgardem/spacefox/pobredinheirinho/web/HttpServer.java
@@ -0,0 +1,67 @@
+package br.com.talesgardem.spacefox.pobredinheirinho.web;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HttpServer {
+    private static final int THREAD_POOL_SIZE = 10;
+    private final int port;
+    private ServerSocket serverSocket;
+    private volatile boolean running = false;
+
+    public HttpServer(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        running = true;
+        try (ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE)) {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server started on port " + port);
+
+            while (running) {
+                Socket clientSocket = serverSocket.accept();
+                threadPool.execute(() -> handleClient(clientSocket));
+            }
+        } catch (IOException e) {
+            if (running) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stop() throws IOException {
+        running = false;
+        if (serverSocket != null && !serverSocket.isClosed()) {
+            serverSocket.close();
+        }
+        System.out.println("Servidor parado.");
+    }
+
+    void handleClient(Socket clientSocket) {
+        try (clientSocket;
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))
+        ) {
+            StringBuilder clientInputLine = new StringBuilder();
+            while (true) {
+                String content = in.readLine();
+                if (content == null || content.isEmpty()) break; // end of message
+                clientInputLine.append(content);
+            }
+            if (clientInputLine.isEmpty()) return;
+            String[] parts = clientInputLine.toString().split(" ");
+            if (parts.length < 2) return; // invalid message
+            String method = parts[0];
+            String path = parts[1];
+
+            // TODO: handle routing
+        } catch (IOException e) {
+            System.out.println("Error closing socket:");
+            e.printStackTrace();
+        }
+    }
+}
